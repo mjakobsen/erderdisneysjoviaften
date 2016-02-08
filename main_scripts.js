2db735a3a871c218b2fcae5e7e7766dd9f3801a5
Michael Jakobsen <michaelljakobsen@gmail.com>
2016-02-08 23:53:53 +0100

    Added basic functionality, no style or prettyup

--- main_scripts.js
+++ main_scripts.js
@@ -1,3 +1,45 @@
+var today = moment().format("YYYY-MM-DD").toString()
+var disneyTitle = "disney sjov";
+
+var isThereDisneyTonight = undefined;
+
+function checkForDisney(response) {
+	_.forEach(response.Data[0].Broadcasts, function(value, key, list) {
+		if(value.Title.toLowerCase().indexOf(disneyTitle) >= 0) {
+			isThereDisneyTonight = {endDate: value.AnnouncedEndTime, startDate: value.AnnouncedStartTime}
+		};
+	});
+};
+
+function setYes(startTime) {
+	$("#main").html("Ja der er disney sjov, og det starter klokken: " + startTime);
+}
+
+function setNo() {
+	$("#main").html("Desværre ikke i dag");
+}
+
+function setTooLate(endTime) {
+	$("#main").html("Der var disney sjov i dag, men det er desværre for sent det sluttede klokken: " + endTime);
+}
+
 ($(document).ready(function(){
-	$("#main").append("<p>Hello world</p>");
+	$.ajax({
+	  url: "http://www.dr.dk/mu/Schedule/" + today + "%40dr.dk/mas/whatson/channel/DR1",
+	}).success(function(response) {
+		checkForDisney(response);
+		if(isThereDisneyTonight !== undefined) {
+			var endDateTime = moment(isThereDisneyTonight.endDate);
+			var now = moment();
+			if(now.isBefore(endDateTime)){
+				var startTime = moment(isThereDisneyTonight.startDate).format("HH:mm");
+				setYes(startTime);
+			} else {
+				var endTime = endDateTime.format("HH:mm");
+				setTooLate(endTime);
+			}			
+		} else {
+			setNo();
+		}
+	});
 }));
